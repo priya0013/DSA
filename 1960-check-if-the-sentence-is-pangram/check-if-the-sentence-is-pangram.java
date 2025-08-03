4e0de96af6c8ee22b313cf32defac9be65e46c22
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2025-08-03 15:44:08 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (99.88%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkIfPangram(String s) {
+        for(char i='a';i<='z';i++){
+            if(!s.contains(String.valueOf(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
