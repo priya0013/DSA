340e536f1410dd177c7233d9c750292759fc59fc
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2025-08-11 21:46:52 +0530

    Time: 19 ms (78.49%) | Memory: 106.3 MB (5.38%) - LeetSync

---
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
@@ -0,0 +1,25 @@
+class Solution {
+    static final int mod=1000000007;
+    public int[] productQueries(int n, int[][] queries) {
+        List<Integer> ls=new ArrayList<>();
+         int bit = 1;
+        while (bit <= n) {
+            if ((n & bit) != 0) {
+                ls.add(bit);
+            }
+            bit <<= 1;
+        }
+        int res[]=new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            long pro=1;
+            int l=queries[i][0];
+            int r=queries[i][1];
+            for(int j=l;j<=r;j++){
+                pro=(pro*ls.get(j))%mod;
+            }
+            res[i]=(int)pro;
+
+        }
+        return res;
+    }
+}
