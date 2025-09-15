20c4c8aa15c90566ebc0b2e25d54639cb2a35407
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2025-09-15 11:56:34 +0530

    Time: 113 ms (56.63%) | Memory: 141.8 MB (93.75%) - LeetSync

---
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& rec) {
+        long long cnt=0;
+        map<double ,long long> mp;
+        for(int i=0;i<rec.size();i++){
+            double r1=(double)rec[i][0]/rec[i][1];
+            mp[r1]++;
+        }
+        for(pair<double,int> p:mp){
+            long long n=p.second;
+            cnt+=(n*(n-1)/2);
+        }
+        return cnt;
+    }
+};
