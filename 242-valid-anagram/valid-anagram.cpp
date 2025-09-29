affaa9e5735e395dd1bafdf886b4994cbf8e95c7
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2025-09-29 10:23:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (48.64%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> cnt(26,0);
+        for(char c:s){
+            cnt[c-'a']++;
+        }
+        for(char c:t){
+            cnt[c-'a']--;
+        }
+        for(int i:cnt){
+            if(i!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
