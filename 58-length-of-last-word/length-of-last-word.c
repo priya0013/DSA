d3f1f8dd50a01ab8449981ab753911915c80c779
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2025-10-09 10:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (32.32%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.c
+++ 58-length-of-last-word/length-of-last-word.c
@@ -1,12 +1,14 @@
 int lengthOfLastWord(char* s) {
     int count=0;
     int i=strlen(s)-1;
-    while(i>=0 && s[i]==' '){
+    while(s[i]==' '){
         i--;
     }
-    while(i>=0 && s[i]!=' '){
+
+    while(i>=0 &&s[i]!=' '){
         count++;
         i--;
-    }
+    } 
+    
     return count;
 }
