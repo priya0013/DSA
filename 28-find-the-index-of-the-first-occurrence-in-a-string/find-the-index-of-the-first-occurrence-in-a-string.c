abb52adf8816548a68f2d8118d960adf52325e0d
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2025-10-09 11:49:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (11.13%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.c
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.c
@@ -1,16 +1,15 @@
 int strStr(char* haystack, char* needle) {
-    int n=strlen(haystack);
-    int m=strlen(needle);
+    int a=strlen(haystack);
+    int b=strlen(needle);
     int j;
-    for(int i=0;i<=n-m;i++){
-        for(j=0;j<m;j++){
+    for(int i=0;i<=a-b;i++){
+        for(j=0;j<b;j++){
             if(haystack[i+j]!=needle[j]){
                 break;
             }
         }
-        if(j==m){
+        if(j==b){
             return i;
-        
         }
     }
         return -1;
