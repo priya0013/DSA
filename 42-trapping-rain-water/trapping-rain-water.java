bab5adee5a2ceaafe088be5213a19cae13f9f679
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-01-29 07:02:41 +0530

    Time: 1 ms (60.62%) | Memory: 48.9 MB (7.44%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,23 +1,24 @@
 class Solution {
     public int trap(int[] h) {
-        int l[]=new int[h.length];
-        int r[]=new int[h.length];
+        int n=h.length;
+        int l[]=new int[n];
+        int r[]=new int[n];
         int max=-1;
-        for(int i=0;i<h.length;i++){
+        for(int i=0;i<n;i++){
             if(h[i]>max){
                 max=h[i];
             }
             l[i]=max;
         }
         max=-1;
-        for(int j=h.length-1;j>=0;j--){
+        for(int j=n-1;j>=0;j--){
             if(h[j]>max){
                 max=h[j];
             }
             r[j]=max;
         }
         int min=0;
-        for(int i=0;i<h.length;i++){
+        for(int i=0;i<n;i++){
             min+=Math.min(l[i],r[i])-h[i];
         }
         return min;
