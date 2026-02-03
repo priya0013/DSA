8dd9c0c040e00db8b5b27ea97379ff9cfa64b90f
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-02-03 22:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (10.08%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -4,13 +4,13 @@ class Solution {
         if(nums[0]>=nums[1]){
             return false;
         }
-        int cnt=1;
+        int cnt=0;
         for(int i=2;i<n;i++){
             if(nums[i-1]==nums[i])return false;
             if((nums[i-2]-nums[i-1])*(nums[i-1]-nums[i])<0){
                 cnt++;
             }
         }
-        return (cnt)==3;
+        return (cnt+1)==3;
     }
 }
