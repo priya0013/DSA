0a6eec91f9fdacd39d374a86b386f10e1f2a51f3
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-05-01 11:41:28 +0530

    Time: 23 ms (87.23%) | Memory: 91.5 MB (96.76%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2,int r, int c, bool &issub) {
+    int m,n;
+    int dfs(vector<vector<int>>& g1, vector<vector<int>>& g2,int i,int j){
+        //int n=g1.size();
+        if(i<0 || j<0 || i>=m || j>=n || g2[i][j]==0){
+            return  true;
+        }
+        g2[i][j]=0;
+        bool sub=g1[i][j]==1;
+        bool up=dfs(g1,g2,i+1,j);
+        bool dow=dfs(g1,g2,i-1,j);
+        bool l=dfs(g1,g2,i,j+1);
 
-    if (r < 0 || c < 0 ||r >= grid2.size() || c >= grid2[0].size()||grid2[r][c]==0 )
-        return;
-    if (grid1[r][c] == 0)
-        issub = false;
-    grid2[r][c] = 0;
+        bool r=dfs(g1,g2,i,j-1);
+        return sub & up & dow&& l& r;
 
-    dfs(grid1, grid2, r + 1, c, issub);
-    dfs(grid1, grid2, r - 1, c, issub);
-    dfs(grid1, grid2, r, c + 1, issub);
-    dfs(grid1, grid2, r, c - 1, issub);
-}
 
+    }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-       
         int cnt=0;
-        for(int r=0;r<grid2.size();r++){
-            for(int c=0;c<grid2[0].size();c++){
-                if(grid2[r][c]==1){
-                     bool issub=true;
-                    dfs(grid1,grid2,r,c,issub);
-                    if(issub){
-                        cnt++;
+        m=grid2.size();
+        n=grid2[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid2[i][j]==1){
+                    if(dfs(grid1,grid2,i,j)){
+                         cnt++;
                     }
                 }
             }
