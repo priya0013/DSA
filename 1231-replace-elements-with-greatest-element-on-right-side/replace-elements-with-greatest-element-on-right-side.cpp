6182085bab9ab83c5e80e8712c16758667750296
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-06-03 15:55:02 +0530

    Time: 0 ms (100.00%) | Memory: 72.7 MB (47.18%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int val=-1;
+        for(int i=arr.size()-1;i>=0;i--){
+                int temp=arr[i];
+                arr[i]=val;
+                if(val<temp){
+                    val=temp;
+                }
+            
+        }
+        return arr;
+    }
+};
