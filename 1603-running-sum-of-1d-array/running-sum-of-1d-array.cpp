f6a90e63d6d955aa94dae5730a9b69b260cb5858
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-06-03 15:26:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (12.78%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        int j=0;
+        for(int i=1;i<nums.size();i++){
+            nums[i]=nums[j]+nums[i];
+            ans.push_back(nums[i]);
+            j++;
+        }
+        return ans;
+    }
+};
