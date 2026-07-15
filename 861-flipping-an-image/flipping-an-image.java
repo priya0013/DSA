8295ad4e99cc33e9fa31f6bde2a7a4552bfe9c35
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-07-15 21:11:51 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (73.73%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.java
+++ 861-flipping-an-image/flipping-an-image.java
@@ -5,23 +5,15 @@ class Solution {
         for(int i=0;i<n;i++){
             int left=0;
             int right=n-1;
-            while(left<right){
-                int temp=image[i][left];
-                image[i][left]=image[i][right];
+            while(left<=right){
+                int temp=image[i][left]^1;
+                image[i][left]=image[i][right]^1;
                 image[i][right]=temp;
                 left++;
                 right--;
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(image[i][j]==0){
-                    image[i][j]=1;
-                }else{
-                    image[i][j]=0;
-                }
-            }
-        }
+       
         return image;
     }
 }
