03b0d327409c90326553c5338d827298bc9d9a8a
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-08-05 15:26:40 +0530

    Time: 34 ms (60.85%) | Memory: 59.1 MB (60.32%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,8 +1,8 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        
         Arrays.sort(nums);
-        int sum=0;
         for(int i=0;i<nums.length-2;i++){
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
@@ -10,12 +10,11 @@ class Solution {
             int l=i+1;
             int r=nums.length-1;
             while(l<r){
-                sum=nums[i]+nums[l]+nums[r];
-                if(sum==0){
-                    res.add(Arrays.asList(nums[i],nums[l],nums[r]));
-
-                
-                while( l<r && nums[l]==nums[l+1]){
+            int sum=nums[i]+nums[l]+nums[r];
+            if(sum==0){
+                ans.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                //this is for remove the duplicates(only distinct elements) for both left and right
+                while(l<r && nums[l]==nums[l+1]){
                     l++;
                 }
                 while(l<r && nums[r]==nums[r-1]){
@@ -23,13 +22,13 @@ class Solution {
                 }
                 l++;
                 r--;
-                }else if(sum<0){
-                    l++;
-                }else{
-                    r--;
-                }
+            }else if(sum<0){
+                l++;
+            }else{
+                r--;
+            }
             }
         }
-        return res;
+        return ans;
     }
 }
