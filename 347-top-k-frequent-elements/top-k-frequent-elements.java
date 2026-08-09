45ed0a04806b6f0abede68a85a6475e0cf416b67
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-08-09 11:01:01 +0530

    Time: 57 ms (5.09%) | Memory: 48.6 MB (8.23%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -2,14 +2,18 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> mp=new HashMap<>();
         int j=0;
+        //enga frequency find panra
         for(int i=0;i<nums.length;i++){
             mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
         }
         int ans[]=new int[k];
+        // upto k length vara than allowed pananu
         while(j<k){
             int max=-1;
             int ele=0;
+            //key oda value athavathu key na->array element value ->how many times repeated that element a=1,2,3
             for(int a:mp.keySet()){
+                //mp.get(a) element oda frequency 1->3time,2->2times vachu max kandupudi
                 if(mp.get(a)>max){
                     max=mp.get(a);
                     ele=a;
