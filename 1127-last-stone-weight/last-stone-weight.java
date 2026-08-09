eb17a3856cadf2e07deaec35d1853bd17e1d28d6
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-08-09 07:59:29 +0530

    Time: 3 ms (4.95%) | Memory: 42.7 MB (70.57%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,17 +1,37 @@
 class Solution {
+    public void reverse(int []stones,int l,int r){
+        //int l=0;
+        r=r-1;
+        while(l<r){
+            int temp=stones[l];
+            stones[l]=stones[r];
+            stones[r]=temp;
+            l++;
+            r--;
+        }
+    }
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int arr:stones){
-            pq.offer(arr);
-        }
-        while(pq.size()>1){
-            int first=pq.poll();  //first largest
-            int second=pq.poll();  //second largest
-            if(first!=second){
-                pq.offer(first-second);
-            }
+        //List<Integer> l=new ArrayList<>();
+        int n=stones.length;
+        
+        while(n>1){
+            Arrays.sort(stones,0,n);
+            reverse(stones,0,n);
+            int first=stones[0];
+            int second=stones[1];
+            
+                for(int i=2;i<n;i++){
+                    stones[i-2]=stones[i];
+                }
+                n = n - 2;
 
+            if (first != second) {
+                int newone = first - second;
+                stones[n] = newone;
+                n++;
+            }
+            
         }
-        return pq.isEmpty()?0:pq.peek();
+        return n==0?0:stones[0];
     }
 }
