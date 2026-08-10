3cc05af0feff5e9830caf2bec9b178c3bcddc0c8
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-08-10 10:27:19 +0530

    Time: 1 ms (97.40%) | Memory: 47.8 MB (12.09%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,28 +1,22 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int r=matrix.length;
-        int c=matrix[0].length;
-        int zero=1;
-        for(int i=0;i<r;i++){
-            if(matrix[i][0]==0){
-                zero=0;
-            }
-            for(int j=1;j<c;j++){
+        int m=matrix.length;
+        int n=matrix[0].length;
+        boolean []r=new boolean[m];
+        boolean []c=new boolean[n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+                    r[i]=true;
+                    c[j]=true;
                 }
             }
         }
-        for(int i=r-1;i>=0;i--){
-            for(int j=c-1;j>=1;j--){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(r[i] || c[j]){
                     matrix[i][j]=0;
                 }
-            }
-            if(zero==0){
-                matrix[i][0]=0;
-
             }
         }
         
