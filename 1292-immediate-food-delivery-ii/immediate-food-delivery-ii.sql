2467f308e645992b1f6ab3c2516ff0f1712af0ed
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-08-12 01:16:18 +0530

    Time: 784 ms (24.69%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
+
+
+#here (order_date=customer_pref_delivery_date)
+#this line return == 1 ,!= 0 then of only first order avg(0+1+0+1)/4==50%
 select round(avg(order_date=customer_pref_delivery_date)*100,2) as immediate_percentage
 from delivery
 where (customer_id,order_date) in(select customer_id,min(order_date) from delivery group by customer_id);
