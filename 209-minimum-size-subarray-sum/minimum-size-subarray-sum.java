ac31de62c797db45e5ed3c0e71ce27a4e34aced5
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-08-21 12:14:27 +0530

    Time: 2 ms (21.27%) | Memory: 68.8 MB (96.42%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,20 +1,16 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        //ithu sliding window than 
-        // apo enna panuna l and r pointer la eduthu flexible size ku window va shrink and expand pananu ok 
         int l=0;
-        int sum=0;
-        //inga min_len na minimum length la than substring irukanu so,min
-        int min_len=Integer.MAX_VALUE;
+        int cursum=0;
+        int minlen=Integer.MAX_VALUE;
         for(int r=0;r<nums.length;r++){
-            sum+=nums[r];
-            while(sum>=target){
-                //window size minimum edukanum
-                min_len=Math.min(min_len,r-l+1);
-                //if window size expand aana left element ahh remove shrink aagum
-                sum=sum-nums[l++];
+            cursum+=nums[r];
+            while(cursum>=target){
+                minlen=Math.min(minlen,r-l+1);
+                cursum-=nums[l];
+                l++;
             }
         }
-        return min_len==Integer.MAX_VALUE?0:min_len;
+        return (minlen==Integer.MAX_VALUE)?0:minlen;
     }
 }
