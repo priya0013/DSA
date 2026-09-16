d47aeda38c43487338721b17e6162c7e469da90f
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-09-16 18:22:11 +0530

    Time: 9 ms (59.89%) | Memory: 46.1 MB (78.82%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,27 +1,25 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int l=0;
-        int r=0;
-        int maxlen=0;
         int maxfreq=0;
+        int maxlen=0;
+        int r=0;
         int freq[]=new int[26];
-        while(r<s.length()){//traverse to all elemments
-            freq[s.charAt(r)-'A']++;//frequency of the character
-            maxfreq=Math.max(maxfreq,freq[s.charAt(r)-'A']);//find which element has highest frequency
-            while((r-l+1)-maxfreq>k){//(r-l+1)ithu vanthu window size, window size thaandi pochuna window invalid=>so enna panrom na window va shrink pananu athuku left remove panitu varanu
+
+        while(r<s.length()){
+            freq[s.charAt(r)-'A']++;
+            maxfreq=Math.max(freq[s.charAt(r)-'A'],maxfreq);
+            while((r-l+1)-maxfreq>k){
                 freq[s.charAt(l)-'A']--;
                 l++;
                 maxfreq=0;
-                for(int i=0;i<26;i++){// agsin high frequency edukanu
+                for(int i=0;i<26;i++){
                     maxfreq=Math.max(maxfreq,freq[i]);
-                    //l++;
                 }
             }
-            if((r-l+1)-maxfreq<=k){//enga window size lees and equal ahh iruntha max length element repeated return pananu
+            if((r-l+1)-maxfreq<=k){
                 maxlen=Math.max(maxlen,r-l+1);
-                
-            }
-            r++;
+            }r++;
         }
         return maxlen;
     }
