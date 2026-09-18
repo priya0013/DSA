803588106ac23206e13daa9aa3db8a3294a58221
PRIYADHARSHINI KATHIRESAN <156561082+priya0013@users.noreply.github.com>
2026-09-18 18:19:40 +0530

    Time: 2 ms (71.63%) | Memory: 42.5 MB (99.46%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.java
@@ -1,11 +1,20 @@
 class Solution {
     public boolean areOccurrencesEqual(String s) {
-        Map<Character,Integer> mp=new HashMap<>();
+        int cnt[]=new int[26];
+        int target=0;
         for(int i=0;i<s.length();i++){
-            mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
-
+            cnt[s.charAt(i)-'a']++;
         }
-        HashSet<Integer> set=new HashSet<>(mp.values());
-        return set.size()==1;
+        for(int x:cnt){
+            if(x==0){
+                continue;
+            }
+            if(target==0){
+                target=x;
+            }else if(target!=x){
+                return false;
+            }
+        }
+        return true;
     }
 }
